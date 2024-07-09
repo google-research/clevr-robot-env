9d381e4a82bffb6654eaca9665d0bdf021695bcb
Florence Cloutier <northeastflo@gmail.com>
2024-07-09 15:40:15 -0400

    More fixes after feedback

--- tasks.py
+++ tasks.py
@@ -1,5 +1,11 @@
 from env import ClevrGridEnv
 
+def state_validation_task(num_scenes, data_dict, colors, direct_comb, directions):
+    return task_data_generation(num_scenes, data_dict, colors, direct_comb, directions)
+
+def kinematics_task(num_scenes, data_dict, colors, direct_comb, directions):
+    return task_data_generation(num_scenes, data_dict, colors, direct_comb, directions, kinematics=True)
+
 def task_data_generation(num_scenes, data_dict, colors, direct_comb, directions, kinematics=False):
     for _ in range(num_scenes):
         env = ClevrGridEnv(clevr_seed=len(data_dict), mujoco_seed=len(data_dict))
