785a183b8667e62d5c3dc33f50479c8570f9e2a7
esraa-saleh <esraa0x61@gmail.com>
2024-07-17 15:35:28 -0400

    add fixes to auto job submission

--- submit_prediction_jobs.py
+++ submit_prediction_jobs.py
@@ -7,72 +7,79 @@ from pathlib import Path
 
 
 def create_script(job_spec_dict, path_to_code, model_path, task_db_path, exp_name, seeds_list):
-    
-
-        
+                
         script_prelims = """#!/bin/bash
-        #SBATCH --job-name={}
-        #SBATCH --output={}
-        #SBATCH --error={}
-        #SBATCH --time={}
-        #SBATCH --mem={}
-        #SBATCH --gres=gpu:{}:{}""".format(exp_name, job_spec_dict["slurm_out_dir"],
+#SBATCH --job-name={}
+#SBATCH --output={}
+#SBATCH --error={}
+#SBATCH --time={}
+#SBATCH --mem={}
+#SBATCH --gres=gpu:{}:{}""".format(exp_name, job_spec_dict["slurm_out_dir"],
                                       job_spec_dict["slurm_error_dir"],
                                       job_spec_dict["time"],
                                       job_spec_dict["mem"],
                                       job_spec_dict["gpu_type"],
                                       job_spec_dict["num_gpus"])
         script_env_prep = """cd {}
-        module --quiet purge
-        module load python/3.10
-        export MUJOCO_GL="osmesa"
-        virtualenv $SLURM_TMPDIR/clevrenv
-        source $SLURM_TMPDIR/clevrenv/bin/activate
-        pip install -r requirements.txt
-        """.format(path_to_code)
+module --quiet purge
+module load python/3.10
+export MUJOCO_GL="osmesa"
+virtualenv $SLURM_TMPDIR/clevrenv
+source $SLURM_TMPDIR/clevrenv/bin/activate
+pip install -r requirements.txt""".format(path_to_code)
         
         script_core = '\n'.join(["python run_llm_prediction_experiment.py {} {} {} {}".format(task_db_path,
                                                             model_path,
                                                             exp_name,
                                                             seed) for seed in seeds_list])
         
-        script = script_prelims + "\n" + script_env_prep + "\n" + script_core
+        script = script_prelims + "\n\n" + script_env_prep + "\n\n" + script_core
         return script
 
-def distribute_jobs(cluster_config_path, path_to_code, model_path, task_db_path, exp_name, num_seeds):
+def get_time_in_seconds(time_str):
+    hours, minutes, seconds = time_str.split(':')
+    return (int(hours)*3600) + (int(minutes)*60) + int(seconds)
+
+def distribute_jobs(cluster_config_path, path_to_code, model_path, task_db_path, exp_name, num_llm_seeds, time_per_exp):
     
     with open(cluster_config_path) as f:
         job_spec_dict = json.load(f)
     
-    #TODO: this won't work ... need to parse time
-    assert (job_spec_dict["time_per_exp"] < job_spec_dict["max_job_time"])
+    time_per_exp = get_time_in_seconds(time_per_exp)
+    max_job_time = get_time_in_seconds(job_spec_dict["time"])
+    
+    assert (time_per_exp < max_job_time)
     # do only 80 percent of actual max workload for job to leave buffer time
-    max_seeds_per_job = math.floor((job_spec_dict["max_job_time"] / job_spec_dict["time_per_exp"])*0.8) 
+    max_seeds_per_job = math.floor((max_job_time / time_per_exp)*0.8) 
     print("Preparing to launch jobs with", max_seeds_per_job, "experiments per job.")
 
-            
-    for i in range(0, num_seeds, max_seeds_per_job):
+    for i in range(0, num_llm_seeds, max_seeds_per_job):
         
-        seeds_list = [s for s in range(i, i + max_seeds_per_job)]
+        seeds_list = [s for s in range(i, min(i + max_seeds_per_job, num_llm_seeds))]
         print("Launching jobs for seeds", seeds_list)
         script_name = "auto_slurm.sh"
         
-        Path(os.path.dirname(job_spec_dict["slurm_out_dir"])).mkdir(parents=True, exist_ok=True)
-        Path(os.path.dirname(job_spec_dict["slurm_error_dir"])).mkdir(parents=True, exist_ok=True)
+        slurm_out_dir_path = Path(job_spec_dict["slurm_out_dir"])
+        Path(slurm_out_dir_path.name).mkdir(parents=True, exist_ok=True)
+        
+        slurm_error_dir_path = Path(job_spec_dict["slurm_error_dir"])
+        Path(slurm_error_dir_path.name).mkdir(parents=True, exist_ok=True)
+        
         script = create_script(job_spec_dict, path_to_code, model_path, task_db_path, exp_name, seeds_list)
         
-        # with open(script_name) as f:
-        #     f.write(script)
+        with open(script_name, "w") as f:
+            f.write(script)
         print(script)
         print("-----------")
         
-        # os.system("bash {}".format(script_name))
-        # os.remove(script_name)
-        # time.sleep(2) # trying to be kind to slurm scheduler
+        os.system("sbatch {}".format(script_name))
+        os.remove(script_name)
+        time.sleep(2) # trying to be kind to slurm scheduler
 
     
 if __name__ in "__main__":
     # Example params:
+    # path_to_code = "~/scratch/code/clevr_robot_env"
     # task_db_path = "datasets/kinematics_db"
     # model_path = "/network/weights/llama.var/llama_3/Meta-Llama-3-8B-Instruct"
     # exp_name = "kinematics_experiment_v0"
@@ -81,15 +88,17 @@ if __name__ in "__main__":
     
     parser = argparse.ArgumentParser()
     
+    parser.add_argument('path_to_code')
     parser.add_argument('task_db_path')
     parser.add_argument('model_path')
     parser.add_argument('exp_name')
     parser.add_argument('cluster_config_path')
     parser.add_argument('num_llm_seeds', type=int)
-    
+    parser.add_argument('time_per_exp') 
+    # this is the time it takes to do predictions for all scenes in the specified db. But without repetition of predictions per single scene.
     
     args = parser.parse_args()
 
-    distribute_jobs(cluster_config_path=args.cluster_config_path, 
+    distribute_jobs(path_to_code=args.path_to_code, cluster_config_path=args.cluster_config_path, 
          model_path=args.model_path, task_db_path=args.task_db_path,
-         exp_name=args.exp_name, num_llm_seeds=args.num_llm_seeds)
+         exp_name=args.exp_name, num_llm_seeds=args.num_llm_seeds, time_per_exp=args.time_per_exp)
