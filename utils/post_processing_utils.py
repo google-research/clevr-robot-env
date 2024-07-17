762cc5600a6d333ce2f33d65fb5a81057163bc33
esraa-saleh <esraa0x61@gmail.com>
2024-07-17 00:45:04 -0400

    fix post processing to report proportions in confusion matrix stats

--- utils/post_processing_utils.py
+++ utils/post_processing_utils.py
@@ -7,19 +7,23 @@ def compute_confusion_matrix(file_path):
     confusion_matrices = []
 
     for item in data["inputs_and_preds"]:
-        answers = item["answers"]
-        gt = item["gt"]
-
-        true_positives = sum(1 for a, g in zip(answers, gt) if a == g == True)
+        answers = item["pred_answers"]
+        gt = item["gt_answers"]
+        assert (len(answers) == len(gt) == len(item["questions"]))
+        
+        true_positives = float(sum(1 for a, g in zip(answers, gt) if a == g == True)) 
         false_positives = sum(1 for a, g in zip(answers, gt) if a == True and g == False)
+        total_positives = true_positives + false_positives
+        
         true_negatives = sum(1 for a, g in zip(answers, gt) if a == g == False)
         false_negatives = sum(1 for a, g in zip(answers, gt) if a == False and g == True)
+        total_negatives = true_negatives + false_negatives
 
         confusion_matrix = {
-            "TP": true_positives,
-            "FP": false_positives,
-            "TN": true_negatives,
-            "FN": false_negatives
+            "TP": true_positives / total_positives,
+            "FP": false_positives / total_positives,
+            "TN": true_negatives / total_negatives,
+            "FN": false_negatives / total_negatives
         }
 
         confusion_matrices.append(confusion_matrix)
